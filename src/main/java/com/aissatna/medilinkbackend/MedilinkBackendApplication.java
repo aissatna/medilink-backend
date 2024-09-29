3ec2bb21afea4e419c0f18913d221680914475d3
aisstana <aissatnabil91@gmail.com>
2024-09-29 15:10:48 +0200

    Init spring-server

---
+++ src/main/java/com/aissatna/medilinkbackend/MedilinkBackendApplication.java
@@ -0,0 +1,13 @@
+package com.aissatna.medilinkbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedilinkBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MedilinkBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/aissatna/medilinkbackend/MedilinkBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aissatna.medilinkbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedilinkBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
